39631032fc8351729a4c22a78936673dba01651a
Ben Argo <ben@benargo.com>
2017-01-23 18:24:00 +0000

    Added prize fund to herb competition

--- resources/views/herb-competition.blade.php
+++ resources/views/herb-competition.blade.php
@@ -15,19 +15,19 @@
         <!-- Styles -->
         <style>
             html, body {
+                background-attachment: fixed;
                 background-color: #636b6f;
                 background-image: url('images/bg.jpg');
-                background-position: center;
+                background-position: top;
+                background-repeat: no-repeat;
                 color: #fff;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
                 height: 100vh;
                 margin: 0;
             }
-
-            .full-height {
-                height: 100vh;
-                background-color: rgba(0, 0, 0, 0.6);
+            strong {
+                font-weight: 600;
             }
 
             /*.flex-center {
@@ -62,38 +62,38 @@
             a {
                 color: #f0f0f0;
             }
-
-            ul.dashed {
-                list-style-type: none;
-            }
-            ul.dashed > li {
-                text-indent: -5px;
-            }
-            ul.dashed > li:before {
-                content: "-";
-                text-indent: -5px;
-            }
         </style>
 
         <!-- Scripts -->
         <script type="text/javascript" src="//wow.zamimg.com/widgets/power.js"></script><script>var wowhead_tooltips = { "colorlinks": true, "iconizelinks": true, "renamelinks": true }</script>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        <div class="flex-center position-ref">
             <div class="content">
-                <div class="title m-b-md">
+                <h1 class="title m-b-md">
                     Herb Competition
-                </div>
+                </h1>
 
                 <p>Between {{ $now }} and {{ $until }} we're running a herbs competition! Gather as many herbs as you can and deposit them into the
                     second bank tab (it's called 'Herbs' too).</p>
-
-                <p>Cash prizes available for the largest contributor of each herb, with a bonus prize for highest overall contributor.</p>
 
                 <h2>Why are we doing this?</h2>
 
                 <p>We need herbs to turn into flasks, potions and vantus runes. This way, each member will get free
                     access to these items on raid nights.</p>
+
+                <div class="prize">
+                    <h2>Prizes</h2>
+                    <p>Cash prizes available for the largest contributor of each herb, with a bonus prize for highest overall contributor.</p>
+                    <p><em>Total prize fund: 20,000 gold</em></p>
+                    <h3>Individual Prizes</h3>
+                    <p><strong>Highest <a href="#" rel="item=124101">Aethril</a> contributor:</strong> 2,000 gold</p>
+                    <p><strong>Highest <a href="#" rel="item=124102">Dreamleaf</a> contributor:</strong> 3,000 gold</p>
+                    <p><strong>Highest <a href="#" rel="item=124104">Fjarnskaggl</a> contrbutor:</strong> 3,000 gold</p>
+                    <p><strong>Highest <a href="#" rel="item=124103">Foxflower</a> contributor:</strong> 3,000 gold</p>
+                    <p><strong>Highest <a href="#" rel="item=124105">Starlight Rose</a> contributor:</strong> 6,000 gold</p>
+                    <p><strong>Highest overall contributor:</strong> 3,000 gold</p>
+                </div><!-- /.prize -->
 
                 <h2>Anything else?</h2>
 
@@ -101,26 +101,26 @@
 
                 <div class="rules">
                     <h2>Rules</h2>
-                    <ul>
+                    <ol>
                         <li>Only herbs from the Broken Isles count:
-                            <ul class="dashed">
+                            <ol type="a">
                                 <li><a href="#" rel="item=124101">Aethril</a></li>
                                 <li><a href="#" rel="item=124102">Dreamleaf</a></li>
                                 <li><a href="#" rel="item=124104">Fjarnskaggl</a></li>
                                 <li><a href="#" rel="item=124103">Foxflower</a></li>
                                 <li><a href="#" rel="item=124105">Starlight Rose</a></li>
                                 <li><a href="#" rel="item=128304">Yseralline Seed</a></li>
-                            </ul>
+                            </ol>
                         </li>
                         <li>For the purposes of calculating highest overall contributor:
-                            <ul class="dashed">
+                            <ol type="a">
                                 <li>Each <a href="#" rel="item=124105">Starlight Rose</a> is weighted as 3x other herbs</li>
-                                <li><a href="#" rel="item=128304">Yseralline Seeds</a> are not counted</li>
-                            </ul>
+                                <li>Each <a href="#" rel="item=128304">Yseralline Seed</a> is weighted as 1/10th of other herbs</li>
+                            </ol>
                         </li>
                         <li>Deposits outside the above times will not be counted</li>
                         <li>Winner will be announced on {{ $winner }} via the Message of the Day and in the <a href="https://www.facebook.com/groups/1422789254626118/">Facebook group</a></li>
-                    </ul>
+                    </ol>
                 </div><!-- /.rules -->
             </div>
         </div>
